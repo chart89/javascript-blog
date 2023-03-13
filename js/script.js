85214db47427d88e7edfcd74529b84f99ed04053
Darek Biernat <kaiju83@outlook.com>
2023-03-13 22:16:55 +0100

    Add code to display correct article

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 'use strict';
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
   
-    /* [DONE] remove class 'active' from all article links  */
-const activeLinks=document.querySelectorAll('.titles a.active')
+    const activeLinks=document.querySelectorAll('.titles a.active')
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
-for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-}
-
-    /* [IN PROGERSS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
+    const activeArticles=document.querySelectorAll('.posts article.active')
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
-    /* [DONE] remove class 'active' from all articles */
-const activeArticles=document.querySelectorAll('.posts article.active')
+   const articleSelector = clickedElement.getAttribute("href");
+   
+    const targetArticle = document.querySelector(articleSelector);
 
-for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
-
-
-   /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
   
 const links = document.querySelectorAll('.titles a');

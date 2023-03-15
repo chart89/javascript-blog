b1921263daf31f0693dc03648b898b229791ef6e
Darek Biernat <kaiju83@outlook.com>
2023-03-15 21:39:14 +0100

    Add script to create list of articles' links

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 const titleClickHandler = function(event){
+    console.log(event);
     event.preventDefault();
     const clickedElement = this;
   
@@ -7,7 +8,7 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-
+    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
     const activeArticles=document.querySelectorAll('.posts article.active')
@@ -22,8 +23,43 @@ const titleClickHandler = function(event){
     targetArticle.classList.add('active');
 }
   
+
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector);
+titleList.innerHTML = '';
+
+  /* for each article */
+const articles = document.querySelectorAll(optArticleSelector);
+
+let html = '';
+for(let article of articles){
+
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    /* insert link into titleList */
+    html = html + linkHTML;
+    
+}
+titleList.innerHTML = html;
 const links = document.querySelectorAll('.titles a');
-  
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
+}
+
+generateTitleLinks();
